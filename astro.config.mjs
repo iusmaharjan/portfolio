9e84feb6568bab2d4fdcbc02889b4d098a9876ba
Ayush Maharjan <ius.maharjan@gmail.com>
2025-05-05 07:18:32 -0700

    Configure astro to use ayushmaharjan.com as site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://iusmaharjan.github.io',
-  base: 'portfolio',
+  site: 'https://www.ayushmaharjan.com',
   vite: {
     plugins: [tailwindcss()]
   }
